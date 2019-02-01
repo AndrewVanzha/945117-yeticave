9255fdd3025a1b9f90e7bd855b80598f9a2d2a25
Андрей <andy10243@mail.ru>
2019-02-01 15:03:40 +0300

    добавление имени и кода
    добавлено имя,
    добавлен php-код с if-else

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 $is_auth = rand(0, 1);
 
-$user_name = ''; // укажите здесь ваше имя
+$user_name = 'Андрей Ванжа'; // укажите здесь ваше имя
 ?>
 <!DOCTYPE html>
 <html lang="ru">
@@ -29,6 +29,22 @@ $user_name = ''; // укажите здесь ваше имя
         <nav class="user-menu">
 
         <!-- здесь должен быть PHP код для показа имени пользователя -->
+		<?php if ($is_auth) { ?>
+			<div class="user-menu__logged">
+				<p><?php $user_name ?></p>
+			</div>
+		<?php } 
+			else { ?>
+				<ul class="user-menu__list">
+					<li class="user-menu__item">
+						<a href="#">Регистрация</a>
+					</li>
+					<li class="user-menu__item">
+						<a href="#">Вход</a>
+					</li>
+				</ul>
+			<?php }
+		?>
 
         </nav>
     </div>
