ece6538ed2812e26685f8746c92f586aeeacd121
Andrew <av199903@mail.ru>
2019-02-05 00:54:04 +0300

    вставлен цикл по товару
    вставил цикл по товару: картинка, наименование, цена и прочее

--- index.php
+++ index.php
@@ -108,16 +108,20 @@ $item_table = [
         </div>
         <ul class="lots__list">
             <!--заполните этот список из массива с товарами-->
+            <?php for ($i=0; $i<count($item_table); $i++) { ?>
             <li class="lots__item lot">
                 <div class="lot__image">
-                    <img src="" width="350" height="260" alt="">
+                    <img src="<?php print($item_table[$i]['URL Картинки']); ?>" width="350" height="260" alt="<?php print($item_table[$i]['Название']); ?>">
                 </div>
                 <div class="lot__info">
                     <span class="lot__category">Название категории</span>
+                    <?php print($item_table[$i]['Категория']); ?>
                     <h3 class="lot__title"><a class="text-link" href="pages/lot.html">Название товара</a></h3>
+                    <?php print($item_table[$i]['Название']); ?>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
+                            <?php print($item_table[$i]['Цена']); ?>
                             <span class="lot__cost">цена<b class="rub">р</b></span>
                         </div>
                         <div class="lot__timer timer">
@@ -126,6 +130,7 @@ $item_table = [
                     </div>
                 </div>
             </li>
+            <?php } ?>
         </ul>
     </section>
 </main>
