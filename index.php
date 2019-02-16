198b4c5cbfcb067742fe753fc934852a7f039494
Andrew <av199903@mail.ru>
2019-02-16 18:25:31 +0300

    Задание функции расчета времени
    сделал отдельную функцию расчета времени

--- index.php
+++ index.php
@@ -3,14 +3,11 @@
 include_once 'data.php';
 include_once 'functions.php';
 
-$current_time = time();
-$nextday = mktime(24, 0, 0);
-$time_gap = floor($nextday - $current_time);
-$time_gap = $time_gap / 60;
-$time_gap = $time_gap / 60;
+$time_gap = (mktime(24, 0, 0) - time()) / 60 / 60;
+$dt = yeti_time(time());
 
 $page_content = include_template('main.php',
-  ['item_type'=>$item_type, 'item_table'=>$item_table, 'wait_time'=>$time_gap]);
+  ['item_type'=>$item_type, 'item_table'=>$item_table, 'wait_time'=>$dt]);
 
   $layout_content = include_template('layout.php', 
   ['item_type'=>$item_type, 'content'=>$page_content, 'title'=>'yeticave',
