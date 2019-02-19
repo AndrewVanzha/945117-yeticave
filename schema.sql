702f5c52835abab2ed4554857dc9ec2497924edb
Andrew <av199903@mail.ru>
2019-02-19 23:02:10 +0300

    БД schema.sql
    создал базу данных schema, 4 таблицы

---
+++ schema.sql
@@ -0,0 +1,148 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.7
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Фев 19 2019 г., 22:57
+-- Версия сервера: 5.7.20
+-- Версия PHP: 7.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `schema`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `schm_bets`
+--
+
+CREATE TABLE `schm_bets` (
+  `id` int(8) UNSIGNED NOT NULL COMMENT 'номер',
+  `date` int(8) UNSIGNED NOT NULL COMMENT 'дата регистрации',
+  `bid_price` int(8) UNSIGNED NOT NULL COMMENT 'цена спроса',
+  `id_user` int(8) UNSIGNED NOT NULL COMMENT 'ссылка на пользователя',
+  `id_lot` int(8) UNSIGNED NOT NULL COMMENT 'ссылка на лот'
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='таблица ставок';
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `schm_category`
+--
+
+CREATE TABLE `schm_category` (
+  `id` int(8) UNSIGNED NOT NULL,
+  `category` char(30) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='таблица категорий';
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `schm_lots`
+--
+
+CREATE TABLE `schm_lots` (
+  `id` int(8) NOT NULL COMMENT 'номер',
+  `date_reg` int(8) UNSIGNED NOT NULL COMMENT 'дата создания',
+  `title` char(8) NOT NULL COMMENT 'название',
+  `description` varchar(255) NOT NULL COMMENT 'описание',
+  `image` varchar(255) NOT NULL COMMENT 'ссылка на изображение',
+  `init_price` int(8) UNSIGNED NOT NULL COMMENT 'начальная цена',
+  `date_end` int(8) UNSIGNED NOT NULL COMMENT 'дата завершения',
+  `bid_inc` int(8) UNSIGNED NOT NULL COMMENT 'шаг ставки',
+  `id_user` int(8) UNSIGNED NOT NULL COMMENT 'ссылка на пользователя',
+  `id_winner` int(8) UNSIGNED NOT NULL COMMENT 'ссылка на победителя',
+  `id_category` int(8) UNSIGNED NOT NULL COMMENT 'ссылка на категорию'
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='таблица лотов';
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `schm_users`
+--
+
+CREATE TABLE `schm_users` (
+  `id` int(8) UNSIGNED NOT NULL COMMENT 'id',
+  `name` varchar(30) NOT NULL COMMENT 'имя',
+  `email` varchar(30) NOT NULL COMMENT 'email',
+  `password` char(32) NOT NULL COMMENT 'пароль',
+  `date_reg` int(10) UNSIGNED NOT NULL COMMENT 'дата регистрации',
+  `contact_info` varchar(255) NOT NULL COMMENT 'контактная инфо',
+  `avatar` varchar(255) NOT NULL COMMENT 'ссылка на аватар',
+  `id_lot` int(8) UNSIGNED NOT NULL COMMENT 'созданные лоты',
+  `id_bet` int(8) UNSIGNED NOT NULL COMMENT 'ставки'
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='пользователи для БД schema';
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `schm_bets`
+--
+ALTER TABLE `schm_bets`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `schm_category`
+--
+ALTER TABLE `schm_category`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `schm_lots`
+--
+ALTER TABLE `schm_lots`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `schm_users`
+--
+ALTER TABLE `schm_users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `schm_bets`
+--
+ALTER TABLE `schm_bets`
+  MODIFY `id` int(8) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'номер';
+
+--
+-- AUTO_INCREMENT для таблицы `schm_category`
+--
+ALTER TABLE `schm_category`
+  MODIFY `id` int(8) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `schm_lots`
+--
+ALTER TABLE `schm_lots`
+  MODIFY `id` int(8) NOT NULL AUTO_INCREMENT COMMENT 'номер';
+
+--
+-- AUTO_INCREMENT для таблицы `schm_users`
+--
+ALTER TABLE `schm_users`
+  MODIFY `id` int(8) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'id';
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
